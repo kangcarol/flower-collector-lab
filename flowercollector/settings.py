aacdbfa5182fec4a2193244171439a1b4b047865
kangcarol <otherhats@gmail.com>
2022-11-11 22:20:07 -0500

    update the requirements.txt with django-environ and boto3, correct placement of secret key

--- flowercollector/settings.py
+++ flowercollector/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wuevaayjy-wn_@h0lhyhm3r@%@h6@3jvxc=3f_wt5htrj5*ej*'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,7 +86,6 @@ DATABASES = {
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': 'db',
         'PORT': 5432,
-        'SECRET KEY': env('SECRET_KEY'),
     }
 }
 
