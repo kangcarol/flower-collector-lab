6da4f6f99c5a9413af2f9e3ca46c29cf7985d755
kangcarol <otherhats@gmail.com>
2022-11-14 10:46:29 -0500

    config deployment2

--- flowercollector/settings.py
+++ flowercollector/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'the-botanists-library.urls'
+ROOT_URLCONF = 'flowercollector.urls'
 
 TEMPLATES = [
     {
